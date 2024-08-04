a81cb65a377ba6aa3b01d89547ac87ada5343a8d
YariSPB <y.paslavskiy@gmail.com>
2024-08-04 17:20:11 +0200

    get order checkout total

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module example/web-service-gin
+module example/caffee-checkout
 
 go 1.22.5
 
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,7 +13,7 @@ type menuItem struct {
 }
 
 type Order struct {
-	Id    int         `json:"Id"`
+	Id    int64       `json:"Id"`
 	Items []OrderItem `json:"menuItems"`
 }
 
@@ -24,8 +25,9 @@ type OrderItem struct {
 func main() {
 	router := gin.Default()
 	router.GET("/orders", getOrders)
+
 	//router.POST("/createOrder", createOrder)
-	//router.GET("/orders/:id", getOrderByID)
+	router.GET("/orders/:id", getOrderTotal)
 
 	router.Run("localhost:8080")
 }
@@ -44,16 +46,54 @@ var order1 = Order{
 	Items: []OrderItem{{"Juice", 2}, {"Americano", 1}},
 }
 
+//total: 6.2
+
 var order2 = Order{
 	Id:    2,
 	Items: []OrderItem{{"Cappuccino", 2}, {"Croissant", 1}},
 }
+
+// total: 7.2
 
 var orders = []Order{order1, order2}
 
 // getOrders responds with the list of all orders as JSON.
 func getOrders(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, orders)
+}
+
+// get total for an order by Id
+func getOrderTotal(c *gin.Context) {
+	id := c.Param("id")
+	var total float64 = 0.0
+	// Loop over the list of orders, looking for
+	// an order whose ID value matches the parameter.
+	for _, order := range orders {
+		var ID, err = strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+		if order.Id == ID {
+			for _, menuItem := range order.Items {
+				total += getPrice(menuItem.Title) * float64(menuItem.Quantity)
+			}
+
+		}
+	}
+
+	c.IndentedJSON(http.StatusOK, total)
+	return
+}
+
+func getPrice(name string) (price float64) {
+	for _, item := range menuItems {
+		if item.Title == name {
+			price = item.Price
+			return
+		}
+	}
+
+	return
 }
 
 /* postAlbums adds an album from JSON received in the request body.
